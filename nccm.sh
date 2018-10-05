7e42d53f8a8917918b0fd5a65db7bb8054e6ab01
Vigneshwaran082 <Vigneshwaran082@users.noreply.github.com>
2018-10-05 12:43:20 +0530

    Update nccm.sh

--- nccm.sh
+++ nccm.sh
@@ -2,12 +2,8 @@
 
 #Author : vigkanda
 
-echo $1
-echo $2
-
-
-set branch_name =$1
-set version_name=$2
+branch_name=$1
+version_name=$2
 
 echo "Branch Name :  $branch_name"
 echo "Version_name : $version_name"
